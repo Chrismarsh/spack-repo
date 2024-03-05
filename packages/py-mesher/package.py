d705e3e2d40203b1746909d8a26e975a6457dcec
Chris Marsh <chrismarsh.c2@gmail.com>
2024-03-04 22:34:55 -0600

    Fully working mesher

--- packages/py-mesher/package.py
+++ packages/py-mesher/package.py
@@ -23,29 +23,45 @@
 from spack.package import *
 
 
-class PyMesher(CMakePackage):
-    """FIXME: Put a proper description of your package here."""
+class PyMesher(PythonPackage):
+    """
+    Mesher is a novel multi-objective unstructured mesh generation software 
+    that allows mesh generation to be generated from an arbitrary number of hydrologically 
+    important features while maintaining a variable spatial resolution. 
+    """
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/Chrismarsh/mesher/"
     url = "https://github.com/Chrismarsh/mesher/archive/refs/tags/2.0.7.tar.gz"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers("github_user1", "github_user2")
+    maintainers("Chrismarsh")
 
-    # FIXME: Add the SPDX identifier of the project's license below.
-    # See https://spdx.org/licenses/ for a list. Upon manually verifying
-    # the license, set checked_by to your Github username.
-    license("UNKNOWN", checked_by="github_user1")
+    license("GPL-3.0-or-later", checked_by="Chrismarsh")
 
     version("2.0.7", sha256="2af40d5fe86ae93cd4f3b999359e47e183f531c6cbb4bada9339c923db79315d")
 
-    # FIXME: Add dependencies if required.
-    depends_on("cgal@5:+header_only")
+    # pyproject.toml
+    depends_on("cmake@3.16:",type="build")
+    depends_on("py-setuptools", type="build")
+    depends_on("py-wheel", type="build")
+    depends_on("py-scikit-build", type="build")
+    depends_on("py-ninja", type="build")
+    depends_on("py-packaging", type="build")
+
+    # setup.py
+    depends_on("vtk+python@9:")
+    depends_on("py-numpy")
+    depends_on("py-matplotlib")
+    depends_on("py-cloudpickle")
+    depends_on("py-metis")
+    depends_on("py-mpi4py")
+    depends_on("py-natsort")
+
+    # cmake build
+    depends_on("cgal@5: +header_only")
     depends_on("metis")
-    depends_on("boost+program_options+filesystem")
-    depends_on("gdal@3.5:")
+    depends_on("boost@1.71.0: +program_options+filesystem")
+    depends_on("gdal@3.5: +python")
+
 
     def cmake_args(self):
         # FIXME: Add arguments other than
