df727e65452dadb95078d04a457a2b65d839fa56
Chris Marsh <chrismarsh.c2@gmail.com>
2024-03-15 19:31:52 -0600

    Update py-mesher build

--- packages/py-mesher/package.py
+++ packages/py-mesher/package.py
@@ -24,12 +24,7 @@ class PyMesher(PythonPackage):
     version("2.0.7", sha256="2af40d5fe86ae93cd4f3b999359e47e183f531c6cbb4bada9339c923db79315d")
 
     # pyproject.toml
-    # depends_on("cmake@3.16:",type="build")
-    # depends_on("py-setuptools", type="build")
-    # depends_on("py-wheel", type="build")
-    depends_on("py-scikit-build", type="build")
-    depends_on("py-ninja", type="build")
-    # depends_on("py-packaging", type="build")
+    depends_on("py-scikit-build-core +pyproject", type="build")
 
     # setup.py
     depends_on("vtk+python@9:")
@@ -39,7 +34,8 @@ class PyMesher(PythonPackage):
     depends_on("py-metis")
     depends_on("py-mpi4py")
     depends_on("py-natsort")
-    # depends_on("py-cmake")
+    depends_on("py-scipy")
+
 
     # cmake build
     depends_on("cgal@5: +header_only")
@@ -47,10 +43,3 @@ class PyMesher(PythonPackage):
     depends_on("boost@1.71.0: +program_options+filesystem")
     depends_on("gdal@3.5: +python")
 
-    def patch(self):
-        setuppy = find(self.stage.source_path, "setup.py")
-        filter_file(r"setup_requires.append\('cmake'\)", "pass", *setuppy) #, ignore_absent=True)
-
-    def global_options(self, spec, prefix):
-        options = ["--cmake-executable /home/chm003/project-ords/spack/opt/spack/linux-rhel8-icelake/gcc-9.3.0/cmake-3.27.9-ounwa246neqkgtgvwlagqo5ompyg56r4/bin/cmake"]
-        return options
