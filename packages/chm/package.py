d80e2dbac95dfa027ea8d894f73403fe09306e03
Chris Marsh <chrismarsh.c2@gmail.com>
2025-01-09 13:55:46 -0600

    fix cgal variant usage

--- packages/chm/package.py
+++ packages/chm/package.py
@@ -40,7 +40,7 @@ class Chm(CMakePackage):
     depends_on("cmake@3.2:", type="build")
 
     depends_on("boost@1.85.0: +system+filesystem+date_time+thread+chrono+regex+iostreams+program_options+mpi+serialization")
-    depends_on("cgal@5:")
+    depends_on("cgal@6: +gmp")
     depends_on("hdf5 +cxx")
     depends_on("netcdf-cxx4@4.3:")
     depends_on("gdal@3.9: +hdf5 +netcdf")
