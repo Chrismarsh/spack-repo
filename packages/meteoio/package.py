69b7f177f56c16628414510571e499bc245cc26d
Chris Marsh <chrismarsh.c2@gmail.com>
2025-02-10 15:35:15 -0600

    clean up meteoio package

--- packages/meteoio/package.py
+++ packages/meteoio/package.py
@@ -1,24 +1,6 @@
-# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install meteoio
-#
-# You can edit this file again by typing:
-#
-#     spack edit meteoio
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 
 from spack.package import *
 
@@ -27,27 +9,16 @@ class Meteoio(CMakePackage):
     """The MeteoIO library aims at making data access easy and safe for numerical 
     simulations in environmental sciences requiring general meteorological data."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.gitlabext.wsl.ch"
+    homepage = "https://meteoio.slf.ch/"
     url = "https://gitlabext.wsl.ch/snow-models/meteoio/-/archive/MeteoIO-2.8.0/meteoio-MeteoIO-2.8.0.tar.gz"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers("github_user1", "github_user2")
+    maintainers("Chrismarsh")
 
-    # FIXME: Add the SPDX identifier of the project's license below.
-    # See https://spdx.org/licenses/ for a list.
-    license("UNKNOWN")
+    license("LGPL-3.0-or-later", checked_by="Chrismarsh")
 
     version("2.8.0", sha256="898bf0d0329000e7ae18064c30ea72362aac447deda0b013ee22e4aa63563efd")
 
-    # FIXME: Add dependencies if required.
-    # depends_on("foo")
-
     def cmake_args(self):
-        # FIXME: Add arguments other than
-        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
-        # FIXME: If not needed delete this function
         cmake_args = []
         cmake_args.append("-Dshared:BOOL=True")
         cmake_args.append("-DPLUGIN_A3DIO:BOOL=False")
