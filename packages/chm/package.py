f720c8745e936efb3bc431d289128d113cd2de69
Chris Marsh <chrismarsh.c2@gmail.com>
2025-05-01 14:14:55 -0600

    chm beta13

--- packages/chm/package.py
+++ packages/chm/package.py
@@ -19,6 +19,7 @@ class Chm(CMakePackage):
     license("GPL-3.0-or-later", checked_by="Chrismarsh")
 
     version("develop", branch="develop", no_cache=True) # don't source cache this git repo
+    version("1.5.0b13", sha256="1758a654e3b7e073ec44bfcd6e2bd0f95fd735371e4dc12853ed0a15413af26d")
     version("1.5.0b12", sha256="84e0fe30ca8a6bfc8cc1be7799ec7a0fafe57a292ba610e09e0271064c6862a8")
     version("1.4.5", sha256="d22f3c25743495029d044f7b536d4cae930e01de275fbac98e2a687da7edc016")
     version("1.4.3", sha256="fa07e2c8c2f88afef4842a14074a4cade8c137bf314b54ee1b0a6640d8aa0d15")
