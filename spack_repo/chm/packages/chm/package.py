98352a5f11b4f38fbbbf3402066f7a932420ee69
Chris Marsh <chrismarsh.c2@gmail.com>
2025-09-03 19:53:28 +0000

    CHM 1.5.0!!! :party

--- spack_repo/chm/packages/chm/package.py
+++ spack_repo/chm/packages/chm/package.py
@@ -21,9 +21,9 @@ class Chm(CMakePackage):
     license("GPL-3.0-or-later", checked_by="Chrismarsh")
 
     version("develop", branch="develop", no_cache=True) # don't source cache this git repo
-    version("1.5.0b20",
-        sha256="65c36299bd31abcb122fe0d7141f600894fd354f09d7bb635a3c39f7c2d8c645",
-        preferred=True
+    version("1.5.0",
+            sha256="a9fb42cd39baeeec9e5e1157b5d9aa787a12d9c3c661b37402444d88852e76d0",
+            preferred=True
     )
     version("1.4.5", sha256="d22f3c25743495029d044f7b536d4cae930e01de275fbac98e2a687da7edc016")
     version("1.4.3", sha256="fa07e2c8c2f88afef4842a14074a4cade8c137bf314b54ee1b0a6640d8aa0d15")
